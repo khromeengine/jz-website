4d4d4c94310a6566df9a5f956467768a549a76c8
jzzz <jianhengzhou003@gmail.com>
2025-11-11 23:45:12 -0800

    css added and init

--- js/header.js
+++ js/header.js
@@ -2,10 +2,10 @@
 
 let pageHeader = `
     <h1>
-         khrome's Development Page
+         Development Page
     </h1>
     <nav class="navigation-bar">
-        <ul>
+        <ul class="list-bar">
             <li><a href="/home">Home</a></li>
             <li><a href="/about">About</a></li>
             <li><a href="/games">Games Directory</a></li>
