6108d309d7887a04baf67322a07f2dd6902e3feb
jzzz <jianhengzhou003@gmail.com>
2025-11-11 19:21:42 -0800

    added scripted header loading

---
+++ js/header.js
@@ -0,0 +1,18 @@
+// Header Replacement
+
+let pageHeader = `
+    <h1>
+         khrome's Development Page
+    </h1>
+    <nav class="navigation-bar">
+        <ul>
+            <li><a href="/home">Home</a></li>
+            <li><a href="/about">About</a></li>
+            <li><a href="/games">Games Directory</a></li>
+            <li><a href="/contact">Contact</a></li>
+            <li><a href="/blog">Blog</a></li>
+        </ul>
+    </nav>
+`;
+
+document.getElementById("page-header").innerHTML = pageHeader
